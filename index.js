ea51e6ea01538e30deabdc6549b5158b0eaee162
maulanacunay <76051887+maulanacunay@users.noreply.github.com>
2020-12-15 22:49:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
+const BotName = 'CUNAY BOT☄'; 
+const instagram = 'https://instagram.com/halu.nay'; 
 const telegram = 'https://t.me/reuzzz'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
